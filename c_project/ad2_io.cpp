40b01be045b1c0dac9418d12b16b5d316047ee9f
joshdellaz <josh.lazaruk@gmail.com>
2022-03-24 13:10:14 -0600

    working basic loopback

--- c_project/ad2_io.cpp
+++ c_project/ad2_io.cpp
@@ -2,6 +2,7 @@ extern "C" {
 #include <digilent/waveforms/dwf.h>
 }
 #include "wavegen.h"
+#include "scope.h"
 
 #include <iostream>
 #include <vector>
@@ -13,6 +14,7 @@ using namespace std;
 
 void sendInfWaveform(void){
     Wavegen wavegen;
+    Scope scope;
 
     HDWF hdwf;
     char szError[512] = {0};
@@ -25,18 +27,24 @@ void sendInfWaveform(void){
     }
 
 
-    int length = 1028;
+    int length = 100;
 
     int channel = 1;
     FUNC function = funcCustom;
     double offset = 0;
-    double frequency = 1000000;
+    double txfreq_hz = 1000000.0;
+    double txfrequency = (txfreq_hz)/((float)length);//note this may be frequency of the repeated entire pattern
     double amplitude = 1;
     double symmetry = 0;
     double wait = 0;
     double run_time = 0;
     int repeat = 0;
     vector<double> vect;
+
+    scope_data rxdata;
+    double rxfrequency = 4000000.0;
+    int rxbuffersize = 8192;
+    double amplitude_range = 2;
 
 
     for(int i = 0; i < length; i++){
@@ -47,6 +55,13 @@ void sendInfWaveform(void){
         }
     }
 
+    scope.open(hdwf, rxfrequency, rxbuffersize, offset, amplitude_range);
 
-    wavegen.generate(hdwf,channel, function, offset, frequency, amplitude, symmetry, wait, run_time, repeat, vect);
+    wavegen.generate(hdwf,channel, function, offset, txfrequency, amplitude, symmetry, wait, run_time, repeat, vect);
+
+    rxdata = scope.record(hdwf, channel, rxfrequency, rxbuffersize);
+    scope.close(hdwf);
+
+    for (auto i: rxdata.buffer)
+        std::cout << i << ' ';
 }
---
+++ c_project/scope.h
@@ -0,0 +1,57 @@
+#ifndef SCOPE_H
+#define SCOPE_H
+/* OSCILLOSCOPE CONTROL FUNCTIONS: open, measure, trigger, record, close */
+
+/* include the necessary libraries */
+#include <vector>
+
+using namespace std;
+
+/* include the constants and the WaveForms function library */
+#ifdef _WIN32
+#include "C:/Program Files (x86)/Digilent/WaveFormsSDK/inc/dwf.h"
+#elif __APPLE__
+#include "/Library/Frameworks/dwf.framework/Headers/dwf.h"
+#else
+#include <digilent/waveforms/dwf.h>
+#endif
+
+/* ----------------------------------------------------- */
+
+class scope_data {
+    public:
+        vector<double> buffer;
+        vector<double> time;
+        //scope_data& operator=(const scope_data&);
+};
+
+// scope_data& scope_data::operator=(const scope_data &data) {
+//     if (this != &data) {
+//         buffer = data.buffer;
+//         time = data.time;
+//     }
+//     return *this;
+// }
+
+/* ----------------------------------------------------- */
+
+class Scope {
+    private:
+        class Trigger_Source {
+            /* trigger source names */
+            public:
+                const TRIGSRC none = trigsrcNone;
+                const TRIGSRC analog = trigsrcDetectorAnalogIn;
+                const TRIGSRC digital = trigsrcDetectorDigitalIn;
+                const TRIGSRC external[5] = {trigsrcNone, trigsrcExternal1, trigsrcExternal2, trigsrcExternal3, trigsrcExternal4};
+        };
+
+    public:
+        Trigger_Source trigger_source;
+        void open(HDWF device_handle, double sampling_frequency = 20e06, int buffer_size = 8192, double offset = 0, double amplitude_range = 5);
+        double measure(HDWF device_handle, int channel);
+        void trigger(HDWF device_handle, bool enable, const TRIGSRC source = trigsrcNone, int channel = 1, double timeout = 0, bool edge_rising = true, double level = 0);
+        scope_data record(HDWF device_handle, int channel, double sampling_frequency=20e06, int buffer_size=8192);
+        void close(HDWF device_handle);
+};
+#endif

894b9c78deb1dea27ba082a8ec22ca09756d379f
joshdellaz <josh.lazaruk@gmail.com>
2022-07-01 02:17:17 -0600

    created test structure

---
+++ c_project/src/stress_test.c
@@ -0,0 +1,29 @@
+//TODO List
+//Should all of these tests have dynamic phase between packets? How to implement that...
+//Determine how to do continuous tx/rx instead of packet-by-packet
+//How might I simulate clock drift over time...
+//Ensure all phases can sync properly in func_test.c
+//Probably do all tests @ 2 samp/bit? Any advantage to 4?
+
+void testSNR(){
+
+}
+
+//Plot a matrix of number of missed packets or unrecovered data as a factor of both fade length and frequency
+//Compare plot to actual data rate
+//Same thing for bursts
+void testFades(){
+
+}
+void testBursts(){
+
+}
+
+//
+void testSpecialCases(){
+
+}
+
+void testAll(){
+
+}
